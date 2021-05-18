76ac9fb7f67e436de9cc4eb8ab5435b8df23400e
Prykon <dariomanoukian@gmail.com>
2021-05-18 11:29:09 -0300

    design logic done; todo: show icons instead of default circles

--- tile/custom-tile.php
+++ tile/custom-tile.php
@@ -74,8 +74,156 @@ class Custom_Church_Health_Tile_Tile
         return $fields;
     }
 
+    private function display_item_divs() {
+        $items = get_option('custom_church_health_icons', null );
+        $item_count = count( $items ) - 2;
+        
+        switch ( $item_count ) {
+            case 1:
+                $grid_template = [1];
+                break;
+            case 2:
+                $grid_template = [
+                    0,0,0,
+                    1,0,1,
+                    0,0,0,
+                ];
+                break;
+            case 3:
+                $grid_template = [
+                    0,1,0,
+                    1,0,1,
+                ];
+                break;
+            case 4:
+                $grid_template = [
+                    1,1,
+                    1,1,
+                ];
+                break;
+            case 5:
+                $grid_template = [
+                    1,0,1,
+                    0,1,0,
+                    1,0,1,
+                ];
+                break;
+            case 6:
+                $grid_template = [
+                    1,0,1,
+                    1,0,1,
+                    1,0,1,
+                ];
+                break;
+            case 7:
+                $grid_template = [
+                    1,0,1,
+                    1,1,1,
+                    1,0,1,
+                ];
+                break;
+            case 8:
+                $grid_template = [
+                    1,1,1,
+                    1,0,1,
+                    1,1,1,
+                ];
+                break;
+            case 9:
+                $grid_template = [
+                    1,1,1,
+                    1,1,1,
+                    1,1,1,
+                ];
+                break;
+        }
+
+        $i = 1;
+        $output = '';
+        foreach ( $grid_template as $grid_item ) {
+            if ( $grid_item === 0 ) {
+                $output .= '<div class="custom-church-health-item"></div>';
+            } else if ( $grid_item === 1 ) {
+                $output .= '<div class="custom-church-health-item" style="background-color:rgba(200, 200, 200, 0.8);border:1px solid rgba(0, 0, 0, 0.8);">' . $i . '</div>';
+                $i++;
+            }
+        }
+        echo $output;
+    }
+
+    private function display_item_css() {
+        $item_count = count( get_option( 'custom_church_health_icons', null ) ) - 2;
+
+        $output = 'display:grid;';
+        switch ( $item_count ) {
+            case 1:
+                $output .= 'grid-template-columns:auto;';
+                break;
+
+            case 4:
+                $output .= 'grid-template-columns:auto auto;';
+                break;            
+
+            default:
+                $output .= 'grid-template-columns:auto auto auto;';
+                break;
+        }
+        $output .= 'justify-content: space-evenly;';
+        echo $output;
+    }
+
     public function dt_add_section( $section, $post_type ) {
-        return;
+        if ( $section === 'custom_church_health_tile' ): ?>
+        <style>
+            .custom-church-health-item {
+                margin: auto;
+                height:75px;
+                width:75px;
+                border-radius: 100%;
+                font-size: 30px;
+                text-align: center;
+            }
+
+            .custom-church-health-circle {
+                height:302px;
+                width:302px;
+                border-radius:100%;
+                border-width: 3px;
+                border-color: darkgray;
+                border-style: dashed;
+                margin:auto;
+            }
+
+            .custom-church-health-grid {
+                height:75%;
+                width:75%;
+                margin-top: 12.5%;
+                margin-left: auto;
+                margin-right: auto;
+                <?php self::display_item_css(); ?>
+            }
+        </style>
+        <div>
+            <div class="custom-church-health-circle">
+                <div class="custom-church-health-grid">
+                    <?php self::display_item_divs(); ?>
+                </div>
+            </div>
+        </div>
+    <?php endif; ?>
+                <!--
+                <div style="display:flex;flex-wrap:wrap;margin-top:10px" class=" js-progress-bordered-box half-opacity">
+                    <?php foreach ( $fields['custom_church_health_tile_multiselect']['default'] as $key => $option ) : ?>
+                        <div class="group-progress-button-wrapper">
+                            <button  class="group-progress-button" id="<?php echo esc_html( $key ) ?>">
+                                <img src="<?php echo esc_html( $option['icon'] ?? "" ) ?>">
+                            </button>
+                            <p><?php echo esc_html( $option['label'] ) ?> </p>
+                        </div>
+                    <?php endforeach; ?>
+                </div>
+                -->
+        <?php
     }
 }
 Custom_Church_Health_Tile_Tile::instance();
